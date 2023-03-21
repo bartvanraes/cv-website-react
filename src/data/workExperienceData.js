7a3e73369bbb58f815f8067a5e86ba91e70d5ad9
bartvanraes <bart.vanraes@gmail.com>
2023-03-21 10:10:09 +0100

    test

--- src/data/workExperienceData.js
+++ src/data/workExperienceData.js
@@ -11,7 +11,7 @@ const workExerperienceData = [
         temporary: false,
         consultancy: false,
         quit: false,
-        outline: 'Following a career guidance program of the VDAB',
+        outline: 'Following a career guidance program of the VDAB TEST',
         responsibilities: ``,
         projects: ``,
         projectManagement: ProjectManagementType.NONE,
