a55749b8370f42138944066b112b9316d864cd30
bartvanraes <bart.vanraes@gmail.com>
2023-04-24 11:03:50 +0200

    title fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
   return (
     <>
     <Head>
-        <title>Create Next App</title>
+        <title>CV Website Bart Vanraes</title>
         <meta name="description" content="CV Website Bart Vanraes" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
